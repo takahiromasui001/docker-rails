dbd64527f2be5c37759798adc20a6f7cd82c1339
Takahiro Masui <takahiro.masui@luxiar.com>
2021-01-03 18:43:33 +0900

    dbcreate

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@ set -e
 
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /myapp/tmp/pids/server.pid
-bin/rails db:create db:migrate db:seed_fu
+bin/rails db:create
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
